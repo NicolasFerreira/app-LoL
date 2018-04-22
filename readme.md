356268adebc92bb9897cd73698c3b2441b3cd043
Nicolas Ferreira <33837699+Nicocofer@users.noreply.github.com>
2018-04-22 23:40:58 +0200

    ajout du rang pour le joueur , de la page d'acceuil , et d'une page recherche -> search

---
+++ readme.md
@@ -0,0 +1,11 @@
+Commands to execute:
+
+npm install async
+npm install express
+npm install express-handlebars
+npm install -g nodemon
+
+
+To run your script
+
+nodemon server.js
--- server.js
+++ server.js
@@ -17,40 +17,54 @@ app.get('/search', function(req, res) {
   var api_key = 'RGAPI-6d1e59a6-58de-4d44-b66a-8414749bf712';
   var recherche = req.query.summoner.toLowerCase();
   var serveur = 'euw1';
-  var URL = 'https://'+ serveur +'.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+ recherche +'?api_key=' + api_key;
+
 
   async.waterfall([
+    // function qui va permettre de récupérer les infos du joueur a partir de son nom d'invocateur
     function(callback) {
+      var URL = 'https://'+ serveur +'.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+ recherche +'?api_key=' + api_key;
       request(URL, function(err, response, body) {
         if(!err && response.statusCode == 200) {
           var json = JSON.parse(body);
           console.log(json);
-          data.id = json.id;
-          data.name = json.name;
-          data.level = json.summonerLevel;
+          data.id = json.id;   // id du joueur
+          data.name = json.name;   // nom du joueur
+          data.level = json.summonerLevel;  // niveau du joueur
+          data.img = json.profileIconId;  //icone du joueur
+          callback(null, data);
+        } else {
+          console.log(err);
+        }
+      });
+    },
+    // function qui recupère l'id du joueur pour avoir les stats ranked 
+    function(data, callback){
+      var URL = 'https://'+ serveur +'.api.riotgames.com/lol/league/v3/positions/by-summoner/'+data.id+'?api_key=' + api_key;
+      request(URL, function(err, response, body) {
+        if(!err && response.statusCode == 200) {
+          var json = JSON.parse(body);
+          console.log(json[0]);
+          data.tier = json[0].tier;
+          data.rank = json[0].rank;
+          data.lp = json[0].leaguePoints;
           callback(null, data);
         } else {
           console.log(err);
         }
       });
     }
-  ],
-  function(err, data) {
-    if(err) {
-      console.log(err);
-      return;
-    }
+    ],
+    function(err, data) {
+      if(err) {
+        console.log(err);
+        return;
+      }
 
-    res.render('index', {
-      /*title: "coucou",
-      body: "hey",*/
-      info: data
+      res.render('search', {
+        info: data
+      });
     });
-  });
 });
-
-
-
 
 
 
